0b075e6abe9c89602159cbdbc08df12b0c16f1c7
Detective-Coder <aaronfrankenfield@gmail.com>
2021-04-19 09:58:07 -0400

    first commit

---
+++ Building.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
